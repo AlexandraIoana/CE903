97013e03bcf83d479e0b97f4f4d50e9dc5c145ec
Andriani Katsouri <Andriani Katsouri>
2016-02-18 11:09:31 +0000

    SignUp page is created

---
+++ SignUp.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SignUp.aspx.cs" Inherits="SignUp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Sign up<br />
+        <br />
+        <asp:Label ID="loginName_lbl" runat="server" Text="Username"></asp:Label>
+        <asp:TextBox ID="loginName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="name_lbl" runat="server" Text="Name"></asp:Label>
+        <asp:TextBox ID="name" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="email_lbl" runat="server" Text="Email address"></asp:Label>
+        <asp:TextBox ID="email" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="password_lbl" runat="server" Text="Password"></asp:Label>
+        <asp:TextBox ID="password" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="register" runat="server" Text="Sign Up" OnClick="register_Click" />
+        <asp:Label ID="error_msg" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SignUp.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SignUp : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    /**store user's details in the database*/
+    protected void register_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            //create a session
+            LoggedUser user = (LoggedUser)Session["User"];
+            if (user == null)
+            {
+                user = new LoggedUser();
+            }
+
+            //save details in a session
+            user.loginName = loginName.Text;
+            user.name = name.Text;
+            user.email = email.Text.ToLower();//must be in lowercase
+            user.password = password.Text;
+
+            Session["User"] = user;
+            bool registerUser = user.SignUp();//store in database
+            if (registerUser == true)
+            {
+                //redirect to a page
+            }
+            else
+            {
+                error_msg.Text = "Something went wrong, enter your details again";
+            }
+
+        }
+    }
+}
