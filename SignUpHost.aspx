efe8c3914a142c0ba611b5a7b936691253e4f3ac
Andriani Katsouri <Andriani Katsouri>
2016-02-23 11:56:11 +0000

    Sign up page for host

---
+++ SignUpHost.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SignUpHost.aspx.cs" Inherits="SignUpHost" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <p>Sign up for Host</p>
+        <asp:Label ID="loginName_lbl" runat="server" Text="Username"></asp:Label>
+        <asp:TextBox ID="loginName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="name_lbl" runat="server" Text="Name"></asp:Label>
+        <asp:TextBox ID="name" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="email_lbl" runat="server" Text="Email address"></asp:Label>
+        <asp:TextBox ID="email" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="password_lbl" runat="server" Text="Password"></asp:Label>
+        <asp:TextBox ID="password" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Label ID="contactNumber_lbl" runat="server" Text="Contact number"></asp:Label>
+        <asp:TextBox ID="contactNumber" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="registerHost" runat="server" Text="Sign Up" OnClick="register_Click" />
+        <asp:Label ID="error_msg" runat="server"></asp:Label>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ SignUpHost.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SignUpHost : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //store host's details in the database
+    protected void register_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            //create a session
+            Host registerHost = (Host)Session["Host"];
+            if (registerHost == null)
+            {
+                registerHost = new Host();
+            }
+
+            //save details in a session
+            registerHost.loginName = loginName.Text;
+            registerHost.name = name.Text;
+            registerHost.email = email.Text.ToLower();//must be in lowercase
+            registerHost.password = password.Text;
+            registerHost.contactNumber = contactNumber.Text;
+
+            Session["Host"] = registerHost;
+            bool registerUser = registerHost.SignUp(registerHost.loginName, registerHost.name,
+                registerHost.email,registerHost.password,registerHost.contactNumber);//store in database
+            //bool registerUser = registerHost.SignUp();//store in database
+
+            if (registerUser == true)
+            {
+                //redirect to a page - upload property details
+                Response.Redirect("AddProperty.aspx");
+            }
+            else
+            {
+                error_msg.Text = "Something went wrong, enter your details again";
+            }
+
+        }
+    }
+}
