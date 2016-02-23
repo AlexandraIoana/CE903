89fe4d2c66bb4ab0881958781d0489325aa8f7b2
Andriani Katsouri <Andriani Katsouri>
2016-02-23 11:59:23 +0000

    Property page, Host can upload property after registration

---
+++ AddProperty.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AddProperty.aspx.cs" Inherits="AddProperty" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <p>Add Property</p>
+        <asp:Label ID="propertyName_lbl" runat="server" Text="Property name"></asp:Label>
+        <asp:TextBox ID="name" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="location_lbl" runat="server" Text="Location"></asp:Label>
+        <asp:TextBox ID="location" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="no_rooms_lbl" runat="server" Text="Number of rooms"></asp:Label>
+        <asp:DropDownList ID="no_rooms" runat="server" AutoPostBack="True"></asp:DropDownList>
+        <br />
+        <asp:Label ID="price_lbl" runat="server" Text="Price"></asp:Label>
+        <asp:TextBox ID="price" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="upload_property" runat="server" Text="Add Property" OnClick="upload_property_Click" />   
+        <asp:Label ID="error_msg" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ AddProperty.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AddProperty : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            //add values to no_rooms ddl
+            for (int i = 1; i < 11; i++)
+            {
+                no_rooms.Items.Add(i.ToString());
+            }
+        }
+    }
+
+    //store property's details to the database
+    protected void upload_property_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            //create session
+            Property addProperty = (Property)Session["Property"];
+            Host host = (Host)Session["Host"];
+            if (addProperty == null)
+            {
+                addProperty = new Property();
+            }
+
+            //save details in a session
+            addProperty.name = name.Text;
+            addProperty.location = location.Text;
+            addProperty.numberRooms = Convert.ToInt32(no_rooms.SelectedValue);//convert to integer
+            addProperty.price = Convert.ToDouble(price); //convert to double
+           
+            Session["Property"] = addProperty;
+
+            //get host's login name from the session
+            String username = host.loginName;
+            
+            Boolean uploadProperty = host.addProperty(username, addProperty);
+            if (uploadProperty == true)
+            {
+                //redirect to a page              
+            }
+            else
+            {
+                error_msg.Text = "Something went wrong, enter your details again";
+            }
+
+        }
+    }
+}
