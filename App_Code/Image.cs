54be99294edb09f1b65deeea48ce5cc52296f706
Bernardo Trevino <bernardotc94@gmail.com>
2016-03-02 18:15:04 +0000

    Uploading and showing an Image through the site is now possible.

---
+++ App_Code/Image.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Image
+/// </summary>
+public class Image
+{
+    public int id { get; set; }
+    public String name { get; set; }
+    public String contentType { get; set; }
+    public Byte[] bytes { get; set; }
+    public String bytesInBase64 { get; set; }
+
+	public Image()
+	{
+	
+	}
+
+    public Image(String name, String contentType, Byte[] b)
+    {
+        this.name = name.Substring(0, Math.Min(name.Length, 50));
+        this.contentType = contentType;
+        bytes = b;
+        bytesInBase64 = Convert.ToBase64String(bytes, 0, bytes.Length);
+    }
+
+    public Boolean saveImageInDb()
+    {
+        DbConnection db = new DbConnection();
+        SqlConnection connection = db.OpenConnection();
+        String query = "INSERT INTO Images (name, contentType, data) VALUES (@name, @contentType, @data);";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@name", name);
+        command.Parameters.AddWithValue("@ContentType", contentType);
+        command.Parameters.AddWithValue("@Data", bytes);
+        try
+        {
+            command.ExecuteNonQuery();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.Write(ex.Message);
+            return false;
+        }
+    }
+
+    public Boolean loadImageFromDb(int i)
+    {
+        DbConnection db = new DbConnection();
+        SqlConnection connection = db.OpenConnection();
+        String query = "SELECT * FROM Images WHERE id = @id;";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@id", i);
+        SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            id = reader.GetInt32(0);
+            name = reader.GetString(1);
+            contentType = reader.GetString(2);
+            long len = reader.GetBytes(3, 0, null, 0, 0);
+            bytes = new Byte[len];
+            reader.GetBytes(3, 0, bytes, 0, (int)len);
+            bytesInBase64 = Convert.ToBase64String(bytes, 0, bytes.Length);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ TestImages.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class TestImages : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ddlImages.DataSource = GetData("SELECT id, name FROM Images");
+            ddlImages.DataTextField = "Name";
+            ddlImages.DataValueField = "Id";
+            ddlImages.DataBind();
+        }
+    }
+
+    private DataTable GetData(string query)
+    {
+        DataTable dt = new DataTable();
+        string constr = ConfigurationManager.ConnectionStrings["BedAndBreakfastDatabase"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand(query))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    sda.Fill(dt);
+                }
+            }
+            return dt;
+        }
+    }
+
+    protected void FetchImage(object sender, EventArgs e)
+    {
+        string id = ddlImages.SelectedItem.Value;
+        Image1.Visible = id != "0";
+        if (id != "0")
+        {
+            Image i = new Image();
+            i.loadImageFromDb(int.Parse(id));
+            Image1.ImageUrl = "data:image/png;base64," + i.bytesInBase64;
+        }
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        // Read the file and convert it to Byte Array
+        string filePath = FileUpload1.PostedFile.FileName;
+        string filename = Path.GetFileName(filePath);
+        string ext = Path.GetExtension(filename);
+        string contenttype = String.Empty;
+
+        //Set the contenttype based on File Extension
+        switch (ext)
+        {
+            case ".jpg":
+                contenttype = "image/jpg";
+                break;
+            case ".png":
+                contenttype = "image/png";
+                break;
+            case ".gif":
+                contenttype = "image/gif";
+                break;
+        }
+        if (contenttype != String.Empty)
+        {
+
+            Stream fs = FileUpload1.PostedFile.InputStream;
+            BinaryReader br = new BinaryReader(fs);
+            Byte[] bytes = br.ReadBytes((Int32)fs.Length);
+
+            Image image = new Image(filename, contenttype, bytes);
+            if (image.saveImageInDb())
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                lblMessage.Text = "File Uploaded Successfully";
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "File format not recognised." +
+                  " Upload Image formats";
+            }
+        }
+        else
+        {
+            lblMessage.ForeColor = System.Drawing.Color.Red;
+            lblMessage.Text = "File format not recognised." +
+              " Upload Image formats";
+        }
+    }
+}
