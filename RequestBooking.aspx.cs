113aacf29e44f8035893e92487fe59c053d23ea2
NancySamuel <npprab@essex.ac.uk>
2016-03-04 09:52:29 +0000

    Todo: client side validation and UI

---
+++ RequestBooking.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RequestBooking : System.Web.UI.Page
+{
+    LoggedUser loggedUser;
+    Property property;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        loggedUser = (LoggedUser)Session["User"];
+        property = (Property)Session["Property"];
+        if (loggedUser == null)
+        {
+           loggedUser = new LoggedUser();
+        }
+        if (property == null)
+        {
+            property = new Property();
+        }
+    }
+    protected void Request_Booking(object sender, EventArgs e)
+    {
+
+        Booking bookingReq = new Booking();
+        bookingReq.loggedUser = loggedUser;
+        bookingReq.property = property;
+        bookingReq.startDate = startDateLab.Text;
+        bookingReq.endDate = endDateLab.Text;
+        Boolean BookingOk = bookingReq.createBooking(loggedUser, property, bookingReq.startDate, bookingReq.endDate);
+        if (BookingOk)
+        {
+            Response.Redirect("BookingReqConfirmation.aspx");
+        }
+        else
+        {
+            error_msg.Text = "System hit a glitch!, please enter your details again";
+        }
+    }
+}
