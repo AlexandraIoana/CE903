36ede6097715736276bdd815196fbe9136b3783f
NancySamuel <npprab@essex.ac.uk>
2016-03-04 10:08:02 +0000

    need to fix DB

---
+++ BookingReqConfirmation.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BookingReqConfirmation : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    
+    protected void Confirmation(object sender, EventArgs e)
+    {
+        Response.Redirect("Host.aspx");
+        confirmation_msg.Text = "System hit a glitch!, please enter your details again";
+    }
+
+     protected void Redo_Booking(object sender, EventArgs e)
+        {
+             Response.Redirect("RequestBooking.aspx");
+     
+        }
+    }
